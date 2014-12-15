081c068de06df7896a1f0966a9b4c3c7cd9aa811
gailees <gailees@umich.edu>
2014-12-14 21:51:08 -0800

    master detail template in swift + parsearse framework

---
+++ DetailViewController.swift
@@ -0,0 +1,57 @@
+//
+//  DetailViewController.swift
+//  DaaS
+//
+//  Created by David Fontenot on 12/14/14.
+//  Copyright (c) 2014 HackMatch. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    @IBOutlet weak var detailDescriptionLabel: UILabel!
+
+
+    var detailItem: AnyObject? {
+        didSet {
+            // Update the view.
+            self.configureView()
+        }
+    }
+
+    func configureView() {
+        // Update the user interface for the detail item.
+        if let detail: AnyObject = self.detailItem {
+            if let label = self.detailDescriptionLabel {
+                label.text = detail.description
+            }
+        }
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        self.configureView()
+//        
+//        var gameScore = PFObject(className: "GameScore")
+//        gameScore.setObject(1337, forKey: "score")
+//        gameScore.setObject("Sean Plott", forKey: "playerName")
+//        gameScore.saveInBackgroundWithBlock {
+//            (success: Bool!, error: NSError!) -> Void in
+//            if success {
+//                NSLog("Object created with id: \(gameScore.objectId)")
+//            } else {
+//                NSLog("%@", error)
+//            }
+//        }
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
